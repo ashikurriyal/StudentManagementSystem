e918bea369265a725738e03403a33940cb8ca133
nahidraz23 <shantoraz@gmail.com>
2022-03-28 05:13:28 +0600

    Unnecessary line deleted.

--- sMs/src/main/java/com/example/sms/Main.java
+++ sMs/src/main/java/com/example/sms/Main.java
@@ -12,7 +12,7 @@ public class Main extends Application {
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("LogInPage.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 900,700);
-        //scene.getStylesheets().add("login.css");
+
         stage.setTitle("Student Management System");
         stage.setScene(scene);
         stage.show();
