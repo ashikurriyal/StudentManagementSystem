fcddc32af4cc6b5f2cbb6e17da7c60f7836dcdfd
ashikurriyal <riyal.ashikur@gmail.com>
2022-03-30 03:00:32 +0600

    Dashboard Updated with profile icon and Search bar

--- sMs/src/main/java/com/example/sms/Main.java
+++ sMs/src/main/java/com/example/sms/Main.java
@@ -15,7 +15,7 @@ public class Main extends Application {
     public void start(Stage stage) throws IOException {
         primaryStage = stage;
         //FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("LogInPage.fxml"));
-        Parent root = FXMLLoader.load(getClass().getResource("LoginPage.fxml"));
+        Parent root = FXMLLoader.load(getClass().getResource("LogInPage.fxml"));
 
         Scene scene = new Scene(root, 900,700);
         stage.setTitle("Student Management System");
