c97531b479f69fa77b92788e881974f18ed89c5e
ashikurriyal <riyal.ashikur@gmail.com>
2022-03-30 00:19:13 +0600

    Chechking for update

--- sMs/src/main/java/com/example/sms/Main.java
+++ sMs/src/main/java/com/example/sms/Main.java
@@ -2,22 +2,17 @@ package com.example.sms;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
-import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
 import java.io.IOException;
 
 public class Main extends Application {
-    public static Stage primaryStage;
-
     @Override
     public void start(Stage stage) throws IOException {
-        primaryStage = stage;
-        //FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("LogInPage.fxml"));
-        Parent root = FXMLLoader.load(getClass().getResource("LoginPage.fxml"));
-
-        Scene scene = new Scene(root, 900,700);
+        FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("Dashboard.fxml"));
+        Scene scene = new Scene(fxmlLoader.load(), 900,700);
+        //scene.getStylesheets().add("login.css");
         stage.setTitle("Student Management System");
         stage.setScene(scene);
         stage.show();
