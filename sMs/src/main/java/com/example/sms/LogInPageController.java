63d1db5ef2a9fda41a48941a94d2550710708359
nahidraz23 <shantoraz@gmail.com>
2022-03-29 23:44:10 +0600

    Dashboard synced with LoginPage testing successful

--- sMs/src/main/java/com/example/sms/LogInPageController.java
+++ sMs/src/main/java/com/example/sms/LogInPageController.java
@@ -1,4 +1,29 @@
 package com.example.sms;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+
+import java.io.IOException;
+
 public class LogInPageController {
+
+    @FXML
+    private Button btn_signIn;
+
+    @FXML
+    private TextField tf_username;
+
+    @FXML
+    void onclick_signIn(ActionEvent event) {
+        FXMLScene fxmlScene = FXMLScene.load("Dashboard.fxml");
+        Parent root = fxmlScene.root;
+        DashboardController dashboardController = (DashboardController) fxmlScene.controller;
+        Main.primaryStage.setScene(new Scene(root, 900, 700));
+    }
+
 }
--- sMs/src/main/java/com/example/sms/Main.java
+++ sMs/src/main/java/com/example/sms/Main.java
@@ -2,17 +2,22 @@ package com.example.sms;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
 import java.io.IOException;
 
 public class Main extends Application {
+    public static Stage primaryStage;
+
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("Dashboard.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 900,700);
-        //scene.getStylesheets().add("login.css");
+        primaryStage = stage;
+        //FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("LogInPage.fxml"));
+        Parent root = FXMLLoader.load(getClass().getResource("LoginPage.fxml"));
+
+        Scene scene = new Scene(root, 900,700);
         stage.setTitle("Student Management System");
         stage.setScene(scene);
         stage.show();
