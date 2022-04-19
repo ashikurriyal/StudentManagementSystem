98e6d58afa096e2c6a548f5d31b9fa5372cf8e42
ashikurriyal <riyal.ashikur@gmail.com>
2022-04-20 01:35:51 +0600

    Updated VaccineStatus Fxml & Scene Switching

--- sMs/src/main/java/com/example/sms/VaccineStatusController.java
+++ sMs/src/main/java/com/example/sms/VaccineStatusController.java
@@ -1,15 +1,34 @@
 package com.example.sms;
 
 import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
 import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.ChoiceBox;
 import javafx.stage.Stage;
 
 import java.io.IOException;
+import java.net.URL;
+import java.util.ResourceBundle;
 
-public class VaccineStatusController {
+public class VaccineStatusController implements Initializable  /*implements Initializable*/ {
+
+    @FXML
+    private ChoiceBox<String> choicebox1;
+
+    @FXML
+    private ChoiceBox<String> choicebox2;
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+        choicebox1.getItems().addAll("Astrazeneca", "Phizer", "Moderna" , "Johnson & Johnson");
+        choicebox2.getItems().addAll("Astrazeneca", "Phizer", "Moderna" , "Johnson & Johnson");
+
+    }
+
     public void switchToRegistration(ActionEvent event) throws IOException {
         Parent root = FXMLLoader.load(getClass().getResource("Registration.fxml"));
         Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
@@ -17,4 +36,24 @@ public class VaccineStatusController {
         stage.setScene(scene);
         stage.show();
     }
+    @FXML
+    void switchToLogInPage(ActionEvent event) throws IOException {
+        Parent root = FXMLLoader.load(getClass().getResource("LogInPage.fxml"));
+        Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+        Scene scene = new Scene(root);
+        stage.setScene(scene);
+        stage.show();
+
+    }
+
+    @FXML
+    void switchToDashboard(ActionEvent event) throws IOException {
+        Parent root = FXMLLoader.load(getClass().getResource("Dashboard.fxml"));
+        Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+        Scene scene = new Scene(root);
+        stage.setScene(scene);
+        stage.show();
+
+    }
+
 }
